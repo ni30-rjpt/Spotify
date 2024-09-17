f04005eece4ed91a8833804ade5d0e39fc642b7f
Nitish Kumar <stnitish01@gmail.com>
2024-09-17 19:06:33 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -33,17 +33,13 @@ app.use("/api/song", songRoutes);
 
 const __dirname = path.resolve();
 
-if (process.env.NODE_ENV == "production") {
+
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
-} else {
-  app.get("/", (req, res) => {
-    res.send("API Running Sucessfully");
-  });
-}
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
